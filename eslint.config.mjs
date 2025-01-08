d60a223739b5f8fb020a92b8891bec47a39c78dd
hingfei <soonghingfei@hotmail.com>
2025-01-08 17:06:45 +0800

    config: added configs
    - Prettier configs
    - Eslint configs
    - Tailwind configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,59 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next", "next/core-web-vitals", "next/typescript"),
+    {
+        rules: {
+            "consistent-return": "off",
+            "import/order": [
+                "error",
+                {
+                    "groups": [
+                        "builtin",
+                        "external",
+                        "internal",
+                        "parent",
+                        "sibling",
+                        "index",
+                        "object",
+                        "unknown"
+                    ],
+                    "pathGroups": [
+                        {
+                            "pattern": "next**",
+                            "group": "external",
+                            "position": "before"
+                        },
+                        {
+                            "pattern": "react",
+                            "group": "external",
+                            "position": "before"
+                        },
+                        {
+                            "pattern": "@/**",
+                            "group": "internal"
+                        }
+                    ],
+                    "distinctGroup": false,
+                    "pathGroupsExcludedImportTypes": [],
+                    "newlines-between": "never",
+                    "alphabetize": {
+                        "order": "asc"
+                    }
+                }
+            ],
+            "import/prefer-default-export": "off",
+            "lines-between-class-members": ["error", "always", { "exceptAfterSingleLine": true }],
+            "react/no-danger": "off",
+            "react/require-default-props": "off",
+            "react/jsx-first-prop-new-line": [2, "multiline"],
+            "react/jsx-max-props-per-line": [
+                2,
+                { "maximum": 1, "when": "multiline" }
+            ],
+            "@typescript-eslint/no-explicit-any": "off"
+        }
+    }
 ];
 
 export default eslintConfig;
