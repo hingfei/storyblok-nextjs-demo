788f232607855527b2448eb373d6b0215afeee59
hingfei99 <soonghingfei@hotmail.com>
2025-01-09 16:12:31 +0800

    feat: optimized image with image loader utils

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        deviceSizes: [640, 750, 828, 1080, 1200, 1504, 1920, 2048, 3840],
+        loader: "custom",
+        loaderFile: "./src/lib/storyblokImageLoader.js"
+    }
 };
 
 export default nextConfig;
--- src/components/Tour.tsx
+++ src/components/Tour.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { renderRichText, storyblokEditable } from "@storyblok/react/rsc";
+import {
+    renderRichText,
+    storyblokEditable,
+    ISbNode,
+    RichTextSchema,
+} from "@storyblok/react/rsc";
+import Image from "next/image";
 
 const Tour = (props: any) => {
     return (
@@ -10,10 +16,18 @@ const Tour = (props: any) => {
             <h1 className={"text-3xl font-bold md:text-5xl"}>
                 {props.blok.name}
             </h1>
-            <img
+            <Image
                 src={props.blok.main_image.filename}
-                alt=""
+                alt={props.blok.main_image.alt}
+                width={
+                    props.blok.main_image.filename.split("/")[5].split("x")[0]
+                }
+                height={
+                    props.blok.main_image.filename.split("/")[5].split("x")[1]
+                }
                 className={"mt-12 w-full"}
+                sizes={"(max-width: 1538px) 100vw, 1504px"}
+                priority={true}
             />
             <p className={"mt-12 text-lg md:text-2xl md:leading-relaxed"}>
                 {props.blok.introduction}
@@ -21,7 +35,34 @@ const Tour = (props: any) => {
             <div
                 className={"prose mt-16 max-w-none md:prose-lg"}
                 dangerouslySetInnerHTML={{
-                    __html: renderRichText(props?.blok?.body) || "",
+                    __html:
+                        renderRichText(props.blok.body, {
+                            schema: {
+                                ...RichTextSchema,
+                                nodes: {
+                                    ...RichTextSchema.nodes,
+                                    image: (node: ISbNode) => ({
+                                        singleTag: [
+                                            {
+                                                tag: "img",
+                                                attrs: {
+                                                    src: `${node.attrs.src}/m/1504x0/filters:quality(75)`,
+                                                    alt: node.attrs.alt,
+                                                    loading: "lazy",
+                                                    width: node.attrs.src
+                                                        .split("/")[5]
+                                                        .split("x")[0],
+                                                    height: node.attrs.src
+                                                        .split("/")[5]
+                                                        .split("x")[1],
+                                                    class: "w-full"
+                                                },
+                                            },
+                                        ],
+                                    }),
+                                },
+                            },
+                        }) ?? "",
                 }}
             ></div>
         </main>
